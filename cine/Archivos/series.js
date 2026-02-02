772b7a207983e9557e7ec808b71a7510e63a6875
Todesvip <maxterx40@gmail.com>
2026-02-02 17:58:12 -0500

    Create series.js

---
+++ cine/Archivos/series.js
@@ -0,0 +1,27 @@
+const seriesData = [
+    {
+        id: "Rick",
+        titulo: "Rick and Morty",
+        titulo_jp: "",
+        poster: "https://raw.githubusercontent.com/Todesvip/visor-manga/main/cine/imagen/03.jpg",
+        banner: "",
+        rating: "16+",
+        temporada: "Temporada 1",
+        ano: "2013",
+        calidad: "HD",
+        sinopsis: "Después de haber estado desaparecido durante casi 20 años, Rick Sánchez llega de imprevisto a la puerta de la casa de su hija Beth y se va a vivir con ella y su familia utilizando el garaje como su laboratorio personal.",
+        episodios: [
+            { num: 1, nombre: "Episodio 1", duracion: "24m", thumb: "", link: "https://ia600701.us.archive.org/5/items/1-5093953301796356862/1_5093953301796356862.mp4" },
+            { num: 2, nombre: "Episodio 2", duracion: "24m", thumb: "", link: "" },
+            { num: 3, nombre: "Episodio 3", duracion: "24m", thumb: "", link: "" },
+            { num: 4, nombre: "Episodio 4", duracion: "24m", thumb: "", link: "" },
+            { num: 5, nombre: "Episodio 5", duracion: "24m", thumb: "", link: "" },
+            { num: 6, nombre: "Episodio 6", duracion: "24m", thumb: "", link: "" },
+            { num: 7, nombre: "Episodio 7", duracion: "24m", thumb: "", link: "" },
+            { num: 8, nombre: "Episodio 8", duracion: "24m", thumb: "", link: "" },
+            { num: 9, nombre: "Episodio 9", duracion: "24m", thumb: "", link: "" },
+            { num: 10, nombre: "Episodio 10", duracion: "24m", thumb: "", link: "" },
+            { num: 11, nombre: "Episodio 11", duracion: "24m", thumb: "", link: "" }
+        ]
+    }
+];
