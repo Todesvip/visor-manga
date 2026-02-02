57090a4ddc4dc610553cb8f613a2d621ec58b8c9
Todesvip <maxterx40@gmail.com>
2026-02-02 17:58:53 -0500

    Create privados.js

---
+++ cine/Archivos/privados.js
@@ -0,0 +1,66 @@
+const privadosData = [
+    {
+        id: "Sukebe",
+        titulo: "Sukebe Elf Tanbouki",
+        titulo_jp: "スケベエルフ探訪記",
+        poster: "https://veohentai.com/wp-content/uploads/2023/07/Sukebe-Elf-Tanbouki-Episode-1.jpg",
+        banner: "",
+        rating: "+18",
+        temporada: "1 Temporada",
+        ano: "2020",
+        calidad: "HD",
+        sinopsis: "Sukebe Elf Tanbouki, que retrata la vida sexual de una elfa tetona y lasciva, ignorante e ingenua en el ámbito sexual, ¡ha sido adaptada al anime! ―Los elfos Sukebe son una raza legendaria que viste trajes folclóricos obscenos sobre sus cuerpos regordetes y lascivos. -Kazu, una bióloga que continúa investigando a la legendaria elfa lasciva, basándose en las palabras dejadas por su abuelo, finalmente conoció a Maana, una elfa tetona, pura e inocente, lasciva y con un disfraz obsceno, sin conocimientos sexuales. ¡Arrozal! Kazu, quien finalmente no puede soportar las muchas tentaciones inconscientes de la pura Maana, y una elfa Muchiskebe ignorante que se está volviendo lasciva en un abrir y cerrar de ojos.",
+        episodios: [
+            { num: 1, nombre: "Episodio 1", duracion: "24m", thumb: "", sprite: "https://todesvip.github.io/visor-manga/sprit/01/sprite1.vtt", spriteImg: "https://todesvip.github.io/visor-manga/sprit/01/sprite1.jpg", link: "https://ia601707.us.archive.org/34/items/sukebe-elf-tanbouki-1/Sukebe%20Elf%20Tanbouki%20-%201.mkv", subtitulo: "https://todesvip.github.io/visor-manga/subtitles/Sukebe%20Elf%20Tanbouki%20-%201.es.ass" },
+            { num: 2, nombre: "Episodio 2", duracion: "24m", thumb: "", sprite: "https://todesvip.github.io/visor-manga/sprit/01/sprite2.vtt", spriteImg: "https://todesvip.github.io/visor-manga/sprit/01/sprite2.jpg", link: "https://ia601707.us.archive.org/34/items/sukebe-elf-tanbouki-1/Sukebe%20Elf%20Tanbouki%20-%202.mp4", subtitulo: "https://todesvip.github.io/visor-manga/subtitles/Sukebe%20Elf%20Tanbouki%20-%202.es.ass" }
+        ]
+    },
+    {
+        id: "3Ping",
+        titulo: "3Ping Lovers Ippu Nisai no Sekai e Youkoso The Animation",
+        titulo_jp: "3PingLovers!☆一夫二妻の世界へようこそ♪ THE ANIMATION",
+        poster: "https://Dimagemfondo.github.io/visor-imgen/img/folder.jpg",
+        banner: "",
+        rating: "18+",
+        temporada: "Temporada 1",
+        ano: "2015",
+        calidad: "HD",
+        sinopsis: "Basado en el juego erótico de Hadashi Shoujo. Un Otaku atrapado en el mundo del juego RPG Eroge, cuando fue atacado por un monstruo, de repente fue salvado por la heroína que tenía. A partir de ese momento se lanzan a la aventura y disfrute del sexo harén.",
+        episodios: [
+            { num: 1, nombre: "Episodio 1", duracion: "24m", thumb: "", sprite: "https://Dimagemfondo.github.io/visor-imgen/sprite/03/sprite1.vtt", spriteImg: "https://Dimagemfondo.github.io/visor-imgen/sprite/03/sprite1.jpg", link: "https://ia801707.us.archive.org/34/items/sukebe-elf-tanbouki-1/3Ping%20Lovers%20Ippu%20Nisai%20no%20Sekai%20e%20Youkoso%20The%20Animation%20-%201.mkv", subtitulo: "https://Dimagemfondo.github.io/visor-manga/src/3Ping Lovers Ippu Nisai no Sekai e Youkoso The Animation - 1.es.ass" }
+        ]
+    },
+    {
+        id: "Gaki",
+        titulo: "Gaki ni Modotte Yarinaoshi!!!",
+        titulo_jp: "ガキに戻ってやり直し!!!",
+        poster: "https://veohentai.com/wp-content/uploads/2022/08/gaki-ni-modotte-yarinaoshi-1-cv3.png",
+        banner: "",
+        rating: "+18",
+        temporada: "1 Temporada",
+        ano: "2020",
+        calidad: "HD",
+        sinopsis: "Desde niño, las mujeres han acosado a Boku, lo que lo marca, anclado en el pasado e incapaz de seguir adelante con su vida. Un día, Kasumi, su amor de la infancia, lo invita a su boda. Mientras observa a todos a su alrededor llevar una vida adulta feliz, Boku se da cuenta de que lo han dejado atrás, lo que le hace desear poder empezar de cero. De alguna manera, este deseo se hace realidad y termina viajando en el tiempo a su preadolescencia. Pronto conoce a Sera Narumiya, su antigua vecina, quien lo acosó con frecuencia durante sus años escolares. Ante la oportunidad de rehacer su vida, Boku decide afrontar sus problemas del pasado, pero ahora con la mentalidad de un adulto; esto incluye, por supuesto, un enfoque más sexual al tratar con mujeres problemáticas.",
+        episodios: [
+            { num: 1, nombre: "Episodio 1", duracion: "24m", thumb: "", sprite: "https://Dimagemfondo.github.io/visor-imgen/sprite/sprite1.vtt", spriteImg: "https://Dimagemfondo.github.io/visor-imgen/sprite/sprite1.jpg", link: "https://ia601707.us.archive.org/34/items/sukebe-elf-tanbouki-1/Gaki%20ni%20Modotte%20Yarinaoshi%21%21%21%20-%2001.mkv", subtitulo: "https://Dimagemfondo.github.io/visor-imgen/src/Gaki%20ni%20Modotte%20Yarinaoshi!!!%20-%2001.es.ass" },
+            { num: 2, nombre: "Episodio 2", duracion: "24m", thumb: "", sprite: "https://Dimagemfondo.github.io/visor-imgen/sprite/sprite2.vtt", spriteImg: "https://Dimagemfondo.github.io/visor-imgen/sprite/sprite2.jpg", link: "https://ia601707.us.archive.org/34/items/sukebe-elf-tanbouki-1/Gaki%20ni%20Modotte%20Yarinaoshi%21%21%21%20-%2002.mkv", subtitulo: "https://Dimagemfondo.github.io/visor-imgen/src/Gaki%20ni%20Modotte%20Yarinaoshi!!!%20-%2002.es.ass" }
+        ]
+    },
+    {
+        id: "Hachishaku",
+        titulo: "Hachishaku Hachiwa Keraku Meguri: Igyou Kaikitan The Animation",
+        titulo_jp: "",
+        poster: "https://veohentai.com/wp-content/uploads/2022/08/hachishaku-hachiwa-keraku-meguri-igyou-kaikitan-GAPGn.jpg",
+        banner: "",
+        rating: "18+",
+        temporada: "1 Temporada",
+        ano: "2020",
+        calidad: "HD",
+        sinopsis: "Basado en el manga erótico de Jyoka.",
+        episodios: [
+           { num: 1, nombre: "Episodio 1", duracion: "24m", thumb: "", sprite: "https://Dimagemfondo.github.io/visor-imgen/sprite/02/sprite1.vtt", spriteImg: "https://Dimagemfondo.github.io/visor-imgen/sprite/02/sprite1.jpg", link: "https://ia601707.us.archive.org/34/items/sukebe-elf-tanbouki-1/Hachishaku%20Hachiwa%20Keraku%20Meguri%20Igyou%20Kaikitan%20The%20Animation%2001.mkv", subtitulo: "https://Dimagemfondo.github.io/visor-imgen/src/Hachishaku Hachiwa Keraku Meguri Igyou Kaikitan The Animation 01.es.ass" },
+           { num: 2, nombre: "Episodio 2", duracion: "24m", thumb: "", sprite: "https://Dimagemfondo.github.io/visor-imgen/sprite/02/sprite2.vtt", spriteImg: "https://Dimagemfondo.github.io/visor-imgen/sprite/02/sprite2.jpg", link: "https://ia601707.us.archive.org/34/items/sukebe-elf-tanbouki-1/Hachishaku%20Hachiwa%20Keraku%20Meguri%20Igyou%20Kaikitan%20The%20Animation%2002.mkv", subtitulo: "https://Dimagemfondo.github.io/visor-imgen/src/Hachishaku Hachiwa Keraku Meguri Igyou Kaikitan The Animation 03.es.ass" },
+           { num: 3, nombre: "Episodio 3", duracion: "24m", thumb: "", sprite: "https://Dimagemfondo.github.io/visor-imgen/sprite/02/sprite3.vtt", spriteImg: "https://Dimagemfondo.github.io/visor-imgen/sprite/02/sprite3.jpg", link: "https://ia601707.us.archive.org/34/items/sukebe-elf-tanbouki-1/Hachishaku%20Hachiwa%20Keraku%20Meguri%20Igyou%20Kaikitan%20The%20Animation%2003.mkv", subtitulo: "https://Dimagemfondo.github.io/visor-imgen/src/Hachishaku Hachiwa Keraku Meguri Igyou Kaikitan The Animation 02.es.ass" }
+        ]
+    }
+];
