ec252003211248ddcf17934713d87374cba3b8fc
Todesvip <maxterx40@gmail.com>
2026-02-02 17:55:34 -0500

    Update latino.js

--- cine/Archivos/latino.js
+++ cine/Archivos/latino.js
@@ -1,1 +1,55 @@
+const latinoData = [
+    {
+        id: "Overlord",
+        titulo: "Overlord",
+        titulo_jp: "オーバーロード",
+        poster: "https://raw.githubusercontent.com/Todesvip/visor-manga/main/cine/imagen/01.jpg",
+        banner: "",
+        rating: "16+",
+        temporada: "1 Temporada",
+        ano: "2015",
+        calidad: "HD",
+        sinopsis: "Yggdrasil es un popular juego en línea que cierra sus servidores. Sin embargo, Momonga, un poderoso mago, decide no salir del juego. Como resultado, se transforma en un esqueleto y se convierte en el mago más poderoso del mundo. Ahora, sin padres, amigos o lugar en la sociedad, este joven ordinario se esfuerza por dominar el nuevo mundo en el que se ha convertido el juego.",
+        episodios: [
+            { num: 1, nombre: "Episodio 1", duracion: "24m", thumb: "", link: "" },
+            { num: 2, nombre: "Episodio 2", duracion: "24m", thumb: "", link: "" },
+            { num: 3, nombre: "Episodio 3", duracion: "24m", thumb: "", link: "" },
+            { num: 4, nombre: "Episodio 4", duracion: "24m", thumb: "", link: "" },
+            { num: 5, nombre: "Episodio 5", duracion: "24m", thumb: "", link: "" },
+            { num: 6, nombre: "Episodio 6", duracion: "24m", thumb: "", link: "" },
+            { num: 7, nombre: "Episodio 7", duracion: "24m", thumb: "", link: "" },
+            { num: 8, nombre: "Episodio 8", duracion: "24m", thumb: "", link: "" },
+            { num: 9, nombre: "Episodio 9", duracion: "24m", thumb: "", link: "" },
+            { num: 10, nombre: "Episodio 10", duracion: "24m", thumb: "", link: "" },
+            { num: 11, nombre: "Episodio 11", duracion: "24m", thumb: "", link: "" },
+            { num: 12, nombre: "Episodio 12", duracion: "24m", thumb: "", link: "" }
+        ]
+    },
+    {
+        id: "Overlord II",
+        titulo: "Overlord II",
+        titulo_jp: "オーバーロード II",
+        poster: "https://raw.githubusercontent.com/Todesvip/visor-manga/main/cine/imagen/05.jpg",
+        banner: "",
+        rating: "16+",
+        temporada: "Temporada 2",
+        ano: "2018",
+        calidad: "HD",
+        sinopsis: "Lord Ainz Ooal Gown da el primer paso en su plan para expandir su fama por todo el nuevo mundo. En compañía de la pléyade Narberal y bajo la falsa identidad del guerrero Momon, se instalará en la ciudad de E-Rantel y se registrará en el gremio de aventureros.",
+        episodios: [
+            { num: 1, nombre: "Episodio 1", duracion: "24m", thumb: "", link: "" },
+            { num: 2, nombre: "Episodio 2", duracion: "24m", thumb: "", link: "" },
+            { num: 3, nombre: "Episodio 3", duracion: "24m", thumb: "", link: "" },
+            { num: 4, nombre: "Episodio 4", duracion: "24m", thumb: "", link: "" },
+            { num: 5, nombre: "Episodio 5", duracion: "24m", thumb: "", link: "" },
+            { num: 6, nombre: "Episodio 6", duracion: "24m", thumb: "", link: "" },
+            { num: 7, nombre: "Episodio 7", duracion: "24m", thumb: "", link: "" },
+            { num: 8, nombre: "Episodio 8", duracion: "24m", thumb: "", link: "" },
+            { num: 9, nombre: "Episodio 9", duracion: "24m", thumb: "", link: "" },
+            { num: 10, nombre: "Episodio 10", duracion: "24m", thumb: "", link: "" },
+            { num: 11, nombre: "Episodio 11", duracion: "24m", thumb: "", link: "" },
+            { num: 12, nombre: "Episodio 12", duracion: "24m", thumb: "", link: "" }
+        ]
+    }
+];
 
