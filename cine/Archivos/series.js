dec2944ad9d67ed03b24ea862e9ca5b4206cda36
Todesvip <maxterx40@gmail.com>
2026-02-05 23:48:43 -0500

    Update series.js

--- cine/Archivos/series.js
+++ cine/Archivos/series.js
@@ -11,17 +11,17 @@ const seriesData = [
         calidad: "HD",
         sinopsis: "Después de haber estado desaparecido durante casi 20 años, Rick Sánchez llega de imprevisto a la puerta de la casa de su hija Beth y se va a vivir con ella y su familia utilizando el garaje como su laboratorio personal.",
         episodios: [
-            { num: 1, nombre: "Episodio 1", duracion: "24m", thumb: "", link: "https://ia600701.us.archive.org/5/items/1-5093953301796356862/1_5093953301796356862.mp4" },
-            { num: 2, nombre: "Episodio 2", duracion: "24m", thumb: "", link: "" },
-            { num: 3, nombre: "Episodio 3", duracion: "24m", thumb: "", link: "" },
-            { num: 4, nombre: "Episodio 4", duracion: "24m", thumb: "", link: "" },
-            { num: 5, nombre: "Episodio 5", duracion: "24m", thumb: "", link: "" },
-            { num: 6, nombre: "Episodio 6", duracion: "24m", thumb: "", link: "" },
-            { num: 7, nombre: "Episodio 7", duracion: "24m", thumb: "", link: "" },
-            { num: 8, nombre: "Episodio 8", duracion: "24m", thumb: "", link: "" },
-            { num: 9, nombre: "Episodio 9", duracion: "24m", thumb: "", link: "" },
-            { num: 10, nombre: "Episodio 10", duracion: "24m", thumb: "", link: "" },
-            { num: 11, nombre: "Episodio 11", duracion: "24m", thumb: "", link: "" }
+            { num: 1, nombre: "Episodio 1", duracion: "24m", thumb: "", link: "https://ia802901.us.archive.org/21/items/csdd_20260203/1.Piloto.ia.mp4" },
+            { num: 2, nombre: "Episodio 2", duracion: "24m", thumb: "", link: "https://ia802901.us.archive.org/21/items/csdd_20260203/2.El%20cortador%20de%20c%C3%A9sped%20perruno.ia.mp4" },
+            { num: 3, nombre: "Episodio 3", duracion: "24m", thumb: "", link: "https://ia902901.us.archive.org/21/items/csdd_20260203/3.Parque%20anat%C3%B3mico.ia.mp4" },
+            { num: 4, nombre: "Episodio 4", duracion: "24m", thumb: "", link: "https://ia802901.us.archive.org/21/items/csdd_20260203/4.Los%20alien%C3%ADgenas%20de%20los%20giros%20inesperados.ia.mp4" },
+            { num: 5, nombre: "Episodio 5", duracion: "24m", thumb: "", link: "https://ia902901.us.archive.org/21/items/csdd_20260203/5.La%20misi%C3%B3n%20de%20los%20meeseeks.ia.mp4" },
+            { num: 6, nombre: "Episodio 6", duracion: "24m", thumb: "", link: "https://ia802901.us.archive.org/21/items/csdd_20260203/6.Poci%C3%B3n%20de%20Rick%20n_%C2%B0%209.ia.mp4" },
+            { num: 7, nombre: "Episodio 7", duracion: "24m", thumb: "", link: "https://ia902901.us.archive.org/21/items/csdd_20260203/7.Educando%20a%20Gazorpazorp.ia.mp4" },
+            { num: 8, nombre: "Episodio 8", duracion: "24m", thumb: "", link: "https://ia902901.us.archive.org/21/items/csdd_20260203/8.Sesenta%20Rick-nutos.ia.mp4" },
+            { num: 9, nombre: "Episodio 9", duracion: "24m", thumb: "", link: "https://ia802901.us.archive.org/21/items/csdd_20260203/9.Cosas%20necesarias.ia.mp4" },
+            { num: 10, nombre: "Episodio 10", duracion: "24m", thumb: "", link: "https://ia902901.us.archive.org/21/items/csdd_20260203/10.Encuent-Ricks%20en%20la%20Rick-cera%20fase.ia.mp4" },
+            { num: 11, nombre: "Episodio 11", duracion: "24m", thumb: "", link: "https://ia802901.us.archive.org/21/items/csdd_20260203/11.Ricksy%20Business.ia.mp4" }
         ]
     }
 ];
