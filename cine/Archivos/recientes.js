5eb0d8346e75c0f84a03e049a550ed82405fb00b
Todesvip <maxterx40@gmail.com>
2026-04-26 18:29:50 -0500

    Update recientes.js

--- cine/Archivos/recientes.js
+++ cine/Archivos/recientes.js
@@ -333,35 +333,6 @@ const recientesData = [
         ]
     },
     {
-        id: "OSHI",
-        titulo: "OSHI NO KO",
-        titulo_jp: "【推しの子】",
-        poster: "https://noads-hue.github.io/noads/img/OSHI/poster.webp",
-        poster1: "https://noads-hue.github.io/noads/img/OSHI/poster1.webp",
-        poster3: "https://noads-hue.github.io/noads/img/OSHI/poster3.webp",
-        banner: "",
-        logo: "https://noads-hue.github.io/noads/img/OSHI/Logo.webp",
-        rating: "13+",
-        generos: ["Drama", "Misterio", "Psicológico", "Sobrenatural", "Ídolos", "Venganza", "Tsundere", "Seinen"],
-        temporada: "Temporada 1",
-        ano: "2023",
-        calidad: "HD",
-        sinopsis: "Goro renace como hijo de Ai. Tras su brutal muerte, Aqua busca a su padre en el oscuro mundo del espectáculo.",
-        episodios: [
-            { num: 1, nombre: "Episodio 1", duracion: "24m", thumb: "https://noads-hue.github.io/noads/img/OSHI/01.jpg", sprite: "", spriteImg: "", link: "https://ia800609.us.archive.org/3/items/dsodsods/%E3%80%90Oshi%20No%20Ko%E3%80%91%20-%20S01e01.mp4", subtitulo: "https://noads-hue.github.io/noads/img/OSHI/【OSHI NO KO】 - S01E01.es.ass" },
-            { num: 2, nombre: "Episodio 2", duracion: "24m", thumb: "https://noads-hue.github.io/noads/img/OSHI/02.jpg", sprite: "https://noads-hue.github.io/noads/img/OSHI/sprite2.vtt", spriteImg: "https://noads-hue.github.io/noads/img/OSHI/sprite2.jpg", link: "https://ia800609.us.archive.org/3/items/dsodsods/%E3%80%90Oshi%20No%20Ko%E3%80%91%20-%20S01e02.mp4", subtitulo: "https://noads-hue.github.io/noads/img/OSHI/【OSHI NO KO】 - S01E02.es.ass" },
-            { num: 3, nombre: "Episodio 3", duracion: "24m", thumb: "https://noads-hue.github.io/noads/img/OSHI/03.jpg", sprite: "https://noads-hue.github.io/noads/img/OSHI/sprite3.vtt", spriteImg: "https://noads-hue.github.io/noads/img/OSHI/sprite3.jpg", link: "https://ia800609.us.archive.org/3/items/dsodsods/%E3%80%90Oshi%20No%20Ko%E3%80%91%20-%20S01e03.mp4", subtitulo: "https://noads-hue.github.io/noads/img/OSHI/【OSHI NO KO】 - S01E03.es.ass" },
-            { num: 4, nombre: "Episodio 4", duracion: "24m", thumb: "https://noads-hue.github.io/noads/img/OSHI/04.jpg", sprite: "https://noads-hue.github.io/noads/img/OSHI/sprite4.vtt", spriteImg: "https://noads-hue.github.io/noads/img/OSHI/sprite4.jpg", link: "https://ia800609.us.archive.org/3/items/dsodsods/%E3%80%90Oshi%20No%20Ko%E3%80%91%20-%20S01e04.mp4", subtitulo: "https://noads-hue.github.io/noads/img/OSHI/【OSHI NO KO】 - S01E04.es.ass" },
-            { num: 5, nombre: "Episodio 5", duracion: "24m", thumb: "https://noads-hue.github.io/noads/img/OSHI/05.jpg", sprite: "https://noads-hue.github.io/noads/img/OSHI/sprite5.vtt", spriteImg: "https://noads-hue.github.io/noads/img/OSHI/sprite5.jpg", link: "https://ia800609.us.archive.org/3/items/dsodsods/%E3%80%90Oshi%20No%20Ko%E3%80%91%20-%20S01e05.mp4", subtitulo: "https://noads-hue.github.io/noads/img/OSHI/【OSHI NO KO】 - S01E05.es.ass" },
-            { num: 6, nombre: "Episodio 6", duracion: "24m", thumb: "https://noads-hue.github.io/noads/img/OSHI/06.jpg", sprite: "https://noads-hue.github.io/noads/img/OSHI/sprite6.vtt", spriteImg: "https://noads-hue.github.io/noads/img/OSHI/sprite6.jpg", link: "https://ia800609.us.archive.org/3/items/dsodsods/%E3%80%90Oshi%20No%20Ko%E3%80%91%20-%20S01e06.mp4", subtitulo: "https://noads-hue.github.io/noads/img/OSHI/【OSHI NO KO】 - S01E06.es.ass" },
-            { num: 7, nombre: "Episodio 7", duracion: "24m", thumb: "https://noads-hue.github.io/noads/img/OSHI/07.jpg", sprite: "https://noads-hue.github.io/noads/img/OSHI/sprite7.vtt", spriteImg: "https://noads-hue.github.io/noads/img/OSHI/sprite7.jpg", link: "https://ia800609.us.archive.org/3/items/dsodsods/%E3%80%90Oshi%20No%20Ko%E3%80%91%20-%20S01e07.mp4", subtitulo: "https://noads-hue.github.io/noads/img/OSHI/【OSHI NO KO】 - S01E07.es.ass" },
-            { num: 8, nombre: "Episodio 8", duracion: "24m", thumb: "https://noads-hue.github.io/noads/img/OSHI/08.jpg", sprite: "https://noads-hue.github.io/noads/img/OSHI/sprite8.vtt", spriteImg: "https://noads-hue.github.io/noads/img/OSHI/sprite8.jpg", link: "https://ia800609.us.archive.org/3/items/dsodsods/%E3%80%90Oshi%20No%20Ko%E3%80%91%20-%20S01e08.mp4", subtitulo: "https://noads-hue.github.io/noads/img/OSHI/【OSHI NO KO】 - S01E08.es.ass" },
-            { num: 9, nombre: "Episodio 9", duracion: "24m", thumb: "https://noads-hue.github.io/noads/img/OSHI/09.jpg", sprite: "https://noads-hue.github.io/noads/img/OSHI/sprite9.vtt", spriteImg: "https://noads-hue.github.io/noads/img/OSHI/sprite9.jpg", link: "https://ia800609.us.archive.org/3/items/dsodsods/%E3%80%90Oshi%20No%20Ko%E3%80%91%20-%20S01e09.mp4", subtitulo: "https://noads-hue.github.io/noads/img/OSHI/【OSHI NO KO】 - S01E09.es.ass" },
-            { num: 10, nombre: "Episodio 10", duracion: "24m", thumb: "https://noads-hue.github.io/noads/img/OSHI/10.jpg", sprite: "https://noads-hue.github.io/noads/img/OSHI/sprite10.vtt", spriteImg: "https://noads-hue.github.io/noads/img/OSHI/sprite10.jpg", link: "https://ia800609.us.archive.org/3/items/dsodsods/%E3%80%90Oshi%20No%20Ko%E3%80%91%20-%20S01e10.mp4", subtitulo: "https://noads-hue.github.io/noads/img/OSHI/【OSHI NO KO】 - S01E10.es.ass" },
-            { num: 11, nombre: "Episodio 11", duracion: "24m", thumb: "https://noads-hue.github.io/noads/img/OSHI/11.jpg", sprite: "https://noads-hue.github.io/noads/img/OSHI/sprite11.vtt", spriteImg: "https://noads-hue.github.io/noads/img/OSHI/sprite11.jpg", link: "https://ia800609.us.archive.org/3/items/dsodsods/%E3%80%90Oshi%20No%20Ko%E3%80%91%20-%20S01e11.mp4", subtitulo: "https://noads-hue.github.io/noads/img/OSHI/【OSHI NO KO】 - S01E11.es.ass" }
-        ]
-    },
-    {
         id: "Nukitashi",
         titulo: "Nukitashi - The Animation",
         titulo_jp: "ぬきたし THE ANIMATION",
