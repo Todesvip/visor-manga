cf8045ea04afa33c4e7d52e7b70667e4a02c8198
Todesvip <maxterx40@gmail.com>
2026-04-26 12:51:20 -0500

    Add Kimi no Na wa movie details to peliculas.js

--- cine/Archivos/peliculas.js
+++ cine/Archivos/peliculas.js
@@ -1,1 +1,20 @@
 
+
+
+{
+        id: "Kimi",
+        titulo: "Kimi no Na wa",
+        titulo_jp: "君の名は。",
+        poster: "https://media.kitsu.app/anime/poster_images/11614/original.jpg",
+        banner: "",
+        logo: "https://noads-hue.github.io/noads/img/Kimi/Logo1.png",
+        rating: "13+",
+        generos: ["Drama", "Sobrenatural", "Romance"],
+        temporada: "Pelicula",
+        ano: "2016",
+        calidad: "HD",
+        sinopsis: "La “historia de milagros y amor” gira en torno a Mitsuha y Taki. Mitsuha es una estudiante de secundaria que vive en un pueblo rural situado en las montañas. Su padre es el alcalde y no pasa mucho por casa, ella vive con su hermana pequeña estudiante de primaria y su abuela. Mitsuha tiene una personalidad honesta, pero a ella no le gustan las costumbres del santuario sintoísta de su familia, ni ella ni su padre participan en una campaña electoral. Ella se lamenta de que vive confinada en un pueblo rural y anhela el maravilloso estilo de vida de Tokio. Taki es un estudiante de secundaria que vive en el centro de Tokio. Él pasa su tiempo junto a sus amigos, trabajando a tiempo parcial en un restaurante italiano y está interesado en la arquitectura y las bellas artes. Un día, Mitsuha tiene un sueño en el que ella tiene el cuerpo de un hombre joven. Taki también tiene un sueño parecido y en el que él es una estudiante de secundaria en un pueblo en las montañas en él que nunca ha estado. ¿Cuál es el secreto de sus sueños?",
+        episodios: [
+            { num: 1, nombre: "Episodio 1", duracion: "24m", thumb: "https://noads-hue.github.io/noads/img/kiwi/01.jpg", sprite: "https://noads-hue.github.io/noads/img/Kimi/sprite.vtt", spriteImg: "https://noads-hue.github.io/noads/img/Kimi/sprite01.jpg", link: "https://ia903102.us.archive.org/33/items/vf5v5v/Kimi%20No%20Na%20Wa.mp4", subtitulo: "https://noads-hue.github.io/noads/img/Kimi/Kimi no Na wa.es.ass" }
+        ]
+    },
