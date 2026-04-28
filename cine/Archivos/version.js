7cef01f51b64b84095199a344b5095cb86161e02
Todesvip <maxterx40@gmail.com>
2026-04-27 22:15:10 -0500

    Update version message to prompt immediate update

--- cine/Archivos/version.js
+++ cine/Archivos/version.js
@@ -1,5 +1,5 @@
 {
   "version": 2,
   "link": "https://t.me/Torreoii",
-  "mensaje": "¡Nuevas funciones de temporadas y mejoras en los sprites! Descárgala ahora."
+  "mensaje": "¡Actualizar ahora!."
 }
