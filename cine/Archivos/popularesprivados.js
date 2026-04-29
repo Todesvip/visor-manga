b185b69f1f0faaf9d9ebe76b9befd34ea40562ad
Todesvip <maxterx40@gmail.com>
2026-04-28 20:15:40 -0500

    Update popularesprivados.js

--- cine/Archivos/popularesprivados.js
+++ cine/Archivos/popularesprivados.js
@@ -1,1 +1,100 @@
+const popularesprivadosData = [
+     {
+        id: "Dropout",
+        titulo: "Dropout",
+        titulo_jp: "ドロップアウト",
+        poster: "https://veohentai.com/wp-content/uploads/2022/08/drop-out-1-EvLZG.jpg",
+        banner: "",
+        rating: "18+",
+        generos: ["Esclavas", "Colegialas", "Violación"],
+        temporada: "Temporada 1",
+        ano: "2016",
+        calidad: "HD",
+        sinopsis: "Después de reprobar sus exámenes de ingreso, la hermosa y talentosa estudiante de último año cae a un nuevo nivel y se convierte en una total esclava sexual.",
+        episodios: [
+            { num: 1, nombre: "Episodio 1", duracion: "24m", thumb: "", sprite: "https://Dimagemfondo.github.io/visor-imgen/sprite/12/sprite1.vtt", spriteImg: "https://Dimagemfondo.github.io/visor-imgen/sprite/12/sprite1.jpg", link: "https://ia601707.us.archive.org/34/items/sukebe-elf-tanbouki-1/Dropout%201.mkv", subtitulo: "https://Dimagemfondo.github.io/visor-imgen/src/Dropout 1.es.ass" },
+            { num: 2, nombre: "Episodio 2", duracion: "24m", thumb: "", sprite: "https://Dimagemfondo.github.io/visor-imgen/sprite/12/sprite2.vtt", spriteImg: "https://Dimagemfondo.github.io/visor-imgen/sprite/12/sprite2.jpg", link: "https://ia601707.us.archive.org/34/items/sukebe-elf-tanbouki-1/Dropout%202.mkv", subtitulo: "https://Dimagemfondo.github.io/visor-imgen/src/Dropout 2.es.ass" }
+        ]
+    },
+    {
+        id: "Rance",
+        titulo: "Rance Hikari wo Motomete The Animation",
+        titulo_jp: "ランス01 光をもとめて THE ANIMATION",
+        poster: "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx20783-tAGqOxaFFNaO.png",
+        banner: "",
+        rating: "18+",
+        generos: ["Fantasía", "Harem"],
+        temporada: "Temporada 1",
+        ano: "2014",
+        calidad: "HD",
+        sinopsis: "Adaptación al anime del relanzamiento de 2013 del juego de PC para adultos de AliceSoft de 1989, Rance 01: The Quest for Hikari. Al guerrero bárbaro Rance nada le gusta más que la compañía de mujeres hermosas. Viajando por el continente con su fiel esclavo Sill Plain, Rance acepta trabajos ocasionales para tener la oportunidad de apaciguar su insaciable libido. Aunque sus instintos pervertidos a menudo lo meten en problemas, gente de todas partes elogia sus hazañas heroicas. Mientras realizan una petición para localizar a una joven noble desaparecida, Rance y Sill llegan al majestuoso Reino de Leazas. Mientras se separan para investigar varias pistas, Rance se encuentra ayudando a la gente del pueblo con varios problemas, desde rescatar a una camarera secuestrada hasta purgar un brote de no-muertos. Al acumular fortuna y mujeres, el guerrero finalmente descubre un oscuro secreto dentro del reino al que sólo él puede enfrentarse.",
+        episodios: [
+            { num: 1, nombre: "Episodio 1", duracion: "24m", thumb: "", sprite: "", spriteImg: "", link: "https://ia801707.us.archive.org/34/items/sukebe-elf-tanbouki-1/Rance%2001%20Hikari%20wo%20Motomete%20The%20Animation%20-%201.mkv", subtitulo: "https://Dimagemfondo.github.io/visor-imgen/src/Rance 01 Hikari wo Motomete The Animation - 1.es.ass" },
+            { num: 2, nombre: "Episodio 2", duracion: "24m", thumb: "", sprite: "", spriteImg: "", link: "https://ia801707.us.archive.org/34/items/sukebe-elf-tanbouki-1/Rance%2001%20Hikari%20wo%20Motomete%20The%20Animation%20-%202.mkv", subtitulo: "https://Dimagemfondo.github.io/visor-imgen/src/Rance 01 Hikari wo Motomete The Animation - 2.es.ass" },
+            { num: 3, nombre: "Episodio 3", duracion: "24m", thumb: "", sprite: "", spriteImg: "", link: "https://ia801707.us.archive.org/34/items/sukebe-elf-tanbouki-1/Rance%2001%20Hikari%20wo%20Motomete%20The%20Animation%20-%203.mkv", subtitulo: "https://Dimagemfondo.github.io/visor-imgen/src/Rance 01 Hikari wo Motomete The Animation - 3.es.ass" },
+            { num: 4, nombre: "Episodio 4", duracion: "24m", thumb: "", sprite: "", spriteImg: "", link: "https://ia801707.us.archive.org/34/items/sukebe-elf-tanbouki-1/Rance%2001%20Hikari%20wo%20Motomete%20The%20Animation%20-%204.mkv", subtitulo: "https://Dimagemfondo.github.io/visor-imgen/src/Rance 01 Hikari wo Motomete The Animation - 4.es.ass" }
+
+        ]
+    },
+    {
+        id: "Sakusei",
+        titulo: "Sakusei Byoutou The Animation",
+        titulo_jp: "搾精病棟 THE ANIMATION",
+        poster: "https://veohentai.com/wp-content/uploads/2022/08/sakusei-byoutou-1-cv2.png",
+        banner: "",
+        rating: "18+",
+        generos: ["Enfermeras", "Hospital"],
+        temporada: "Temporada 1",
+        ano: "2021",
+        calidad: "HD",
+        sinopsis: "Basado en la colección CG de Sakusei Kenkyuujo (搾精研究所). Yamada padece una extraña enfermedad y, como resultado, tiene que eyacular una vez cada tres horas. Sin embargo, ahora se rompió ambas manos y terminó en el hospital. Como resultado, tiene que pedir a las enfermeras del hospital que se ocupen de sus necesidades.",
+        episodios: [
+            { num: 1, nombre: "Episodio 1", duracion: "24m", thumb: "", sprite: "https://Dimagemfondo.github.io/visor-imgen/sprite/11/sprite1.vtt", spriteImg: "https://Dimagemfondo.github.io/visor-imgen/sprite/11/sprite1.jpg", link: "https://ia903102.us.archive.org/33/items/vf5v5v/Sakusei%20Byoutou%20The%20Animation%20-%201.mp4", subtitulo: "https://Dimagemfondo.github.io/visor-imgen/src/Sakusei Byoutou The Animation - 1.spa.ass" },
+            { num: 2, nombre: "Episodio 2", duracion: "24m", thumb: "", sprite: "https://Dimagemfondo.github.io/visor-imgen/sprite/11/sprite2.vtt", spriteImg: "https://Dimagemfondo.github.io/visor-imgen/sprite/11/sprite2.jpg", link: "https://ia903102.us.archive.org/33/items/vf5v5v/Sakusei%20Byoutou%20The%20Animation%20-%202.mp4", subtitulo: "https://Dimagemfondo.github.io/visor-imgen/src/Sakusei Byoutou The Animation - 2.spa.ass" },
+            { num: 3, nombre: "Episodio 3", duracion: "24m", thumb: "", sprite: "https://Dimagemfondo.github.io/visor-imgen/sprite/11/sprite3.vtt", spriteImg: "https://Dimagemfondo.github.io/visor-imgen/sprite/11/sprite3.jpg", link: "https://ia903102.us.archive.org/33/items/vf5v5v/Sakusei%20Byoutou%20The%20Animation%20-%203.mp4", subtitulo: "https://Dimagemfondo.github.io/visor-imgen/src/Sakusei Byoutou The Animation - 3.spa.ass" },
+            { num: 4, nombre: "Episodio 4", duracion: "24m", thumb: "", sprite: "https://Dimagemfondo.github.io/visor-imgen/sprite/11/sprite4.vtt", spriteImg: "https://Dimagemfondo.github.io/visor-imgen/sprite/11/sprite4.jpg", link: "https://ia903102.us.archive.org/33/items/vf5v5v/Sakusei%20Byoutou%20The%20Animation%20-%204.mp4", subtitulo: "https://Dimagemfondo.github.io/visor-imgen/src/Sakusei Byoutou The Animation - 4.spa.ass" },
+            { num: 5, nombre: "Episodio 5", duracion: "24m", thumb: "", sprite: "https://Dimagemfondo.github.io/visor-imgen/sprite/11/sprite5.vtt", spriteImg: "https://Dimagemfondo.github.io/visor-imgen/sprite/11/sprite5.jpg", link: "https://ia903102.us.archive.org/33/items/vf5v5v/Sakusei%20Byoutou%20The%20Animation%20-%205.mp4", subtitulo: "https://Dimagemfondo.github.io/visor-imgen/src/Sakusei Byoutou The Animation - 5.spa.ass" },
+            { num: 6, nombre: "Episodio 6", duracion: "24m", thumb: "", sprite: "https://Dimagemfondo.github.io/visor-imgen/sprite/11/sprite6.vtt", spriteImg: "https://Dimagemfondo.github.io/visor-imgen/sprite/11/sprite6.jpg", link: "https://ia903102.us.archive.org/33/items/vf5v5v/Sakusei%20Byoutou%20The%20Animation%20-%206.mp4", subtitulo: "https://Dimagemfondo.github.io/visor-imgen/src/Sakusei Byoutou The Animation - 6.spa.ass" },
+            { num: 7, nombre: "Episodio 7", duracion: "24m", thumb: "", sprite: "https://Dimagemfondo.github.io/visor-imgen/sprite/11/sprite7.vtt", spriteImg: "https://Dimagemfondo.github.io/visor-imgen/sprite/11/sprite7.jpg", link: "https://ia903102.us.archive.org/33/items/vf5v5v/Sakusei%20Byoutou%20The%20Animation%20-%207.mp4", subtitulo: "https://Dimagemfondo.github.io/visor-imgen/src/Sakusei Byoutou The Animation - 7.spa.ass" },
+            { num: 8, nombre: "Episodio 8", duracion: "24m", thumb: "", sprite: "https://Dimagemfondo.github.io/visor-imgen/sprite/11/sprite8.vtt", spriteImg: "https://Dimagemfondo.github.io/visor-imgen/sprite/11/sprite8.jpg", link: "https://ia903102.us.archive.org/33/items/vf5v5v/Sakusei%20Byoutou%20The%20Animation%20-%208.mp4", subtitulo: "https://Dimagemfondo.github.io/visor-imgen/src/Sakusei Byoutou The Animation - 8.spa.ass" },
+            { num: 9, nombre: "Episodio 9", duracion: "24m", thumb: "", sprite: "https://Dimagemfondo.github.io/visor-imgen/sprite/11/sprite9.vtt", spriteImg: "https://Dimagemfondo.github.io/visor-imgen/sprite/11/sprite9.jpg", link: "https://ia903102.us.archive.org/33/items/vf5v5v/Sakusei%20Byoutou%20The%20Animation%20-%209.mp4", subtitulo: "https://Dimagemfondo.github.io/visor-imgen/src/Sakusei Byoutou The Animation - 9.es.ass" },
+            { num: 10, nombre: "Episodio 10", duracion: "24m", thumb: "", sprite: "https://Dimagemfondo.github.io/visor-imgen/sprite/11/sprite10.vtt", spriteImg: "https://Dimagemfondo.github.io/visor-imgen/sprite/11/sprite10.jpg", link: "https://ia903102.us.archive.org/33/items/vf5v5v/Sakusei%20Byoutou%20The%20Animation%20-%2010.mp4", subtitulo: "https://Dimagemfondo.github.io/visor-imgen/src/Sakusei Byoutou The Animation - 10.es.ass" }
+        ]
+    },
+    {
+        id: "Gaki",
+        titulo: "Gaki ni Modotte Yarinaoshi!!!",
+        titulo_jp: "ガキに戻ってやり直し!!!",
+        poster: "https://veohentai.com/wp-content/uploads/2022/08/gaki-ni-modotte-yarinaoshi-1-cv3.png",
+        banner: "",
+        rating: "18+",
+        generos: ["Shota", "Loli", "Harem", "Sobrenatural"],
+        temporada: "Temporada 1",
+        ano: "2020",
+        calidad: "HD",
+        sinopsis: "Desde niño, las mujeres han acosado a Boku, lo que lo marca, anclado en el pasado e incapaz de seguir adelante con su vida. Un día, Kasumi, su amor de la infancia, lo invita a su boda. Mientras observa a todos a su alrededor llevar una vida adulta feliz, Boku se da cuenta de que lo han dejado atrás, lo que le hace desear poder empezar de cero. De alguna manera, este deseo se hace realidad y termina viajando en el tiempo a su preadolescencia. Pronto conoce a Sera Narumiya, su antigua vecina, quien lo acosó con frecuencia durante sus años escolares. Ante la oportunidad de rehacer su vida, Boku decide afrontar sus problemas del pasado, pero ahora con la mentalidad de un adulto; esto incluye, por supuesto, un enfoque más sexual al tratar con mujeres problemáticas.",
+        episodios: [
+            { num: 1, nombre: "Episodio 1", duracion: "24m", thumb: "", sprite: "https://Dimagemfondo.github.io/visor-imgen/sprite/sprite1.vtt", spriteImg: "https://Dimagemfondo.github.io/visor-imgen/sprite/sprite1.jpg", link: "https://ia903102.us.archive.org/33/items/vf5v5v/Gaki%20Ni%20Modotte%20Yarinaoshi!!!%20-%2001.mp4", subtitulo: "https://Dimagemfondo.github.io/visor-imgen/src/Gaki%20ni%20Modotte%20Yarinaoshi!!!%20-%2001.es.ass" },
+            { num: 2, nombre: "Episodio 2", duracion: "24m", thumb: "", sprite: "https://Dimagemfondo.github.io/visor-imgen/sprite/sprite2.vtt", spriteImg: "https://Dimagemfondo.github.io/visor-imgen/sprite/sprite2.jpg", link: "https://ia903102.us.archive.org/33/items/vf5v5v/Gaki%20Ni%20Modotte%20Yarinaoshi!!!%20-%2002.mp4", subtitulo: "https://Dimagemfondo.github.io/visor-imgen/src/Gaki%20ni%20Modotte%20Yarinaoshi!!!%20-%2002.es.ass" }
+        ]
+    },
+    {
+        id: "Hachishaku",
+        titulo: "Hachishaku Hachiwa Keraku Meguri: Igyou Kaikitan The Animation",
+        titulo_jp: "",
+        poster: "https://veohentai.com/wp-content/uploads/2022/08/hachishaku-hachiwa-keraku-meguri-igyou-kaikitan-GAPGn.jpg",
+        banner: "",
+        rating: "18+",
+        generos: ["Sobrenatural", "Terror", "Tetonas", "Shota", "Monstruos"],
+        temporada: "Temporada 1",
+        ano: "2020",
+        calidad: "HD",
+        sinopsis: "Basado en el manga erótico de Jyoka.",
+        episodios: [
+           { num: 1, nombre: "Episodio 1", duracion: "24m", thumb: "", sprite: "https://Dimagemfondo.github.io/visor-imgen/sprite/02/sprite1.vtt", spriteImg: "https://Dimagemfondo.github.io/visor-imgen/sprite/02/sprite1.jpg", link: "https://ia601707.us.archive.org/34/items/sukebe-elf-tanbouki-1/Hachishaku%20Hachiwa%20Keraku%20Meguri%20Igyou%20Kaikitan%20The%20Animation%2001.mkv", subtitulo: "https://Dimagemfondo.github.io/visor-imgen/src/Hachishaku Hachiwa Keraku Meguri Igyou Kaikitan The Animation 01.es.ass" },
+           { num: 2, nombre: "Episodio 2", duracion: "24m", thumb: "", sprite: "https://Dimagemfondo.github.io/visor-imgen/sprite/02/sprite2.vtt", spriteImg: "https://Dimagemfondo.github.io/visor-imgen/sprite/02/sprite2.jpg", link: "https://ia601707.us.archive.org/34/items/sukebe-elf-tanbouki-1/Hachishaku%20Hachiwa%20Keraku%20Meguri%20Igyou%20Kaikitan%20The%20Animation%2002.mkv", subtitulo: "https://Dimagemfondo.github.io/visor-imgen/src/Hachishaku Hachiwa Keraku Meguri Igyou Kaikitan The Animation 03.es.ass" },
+           { num: 3, nombre: "Episodio 3", duracion: "24m", thumb: "", sprite: "https://Dimagemfondo.github.io/visor-imgen/sprite/02/sprite3.vtt", spriteImg: "https://Dimagemfondo.github.io/visor-imgen/sprite/02/sprite3.jpg", link: "https://ia601707.us.archive.org/34/items/sukebe-elf-tanbouki-1/Hachishaku%20Hachiwa%20Keraku%20Meguri%20Igyou%20Kaikitan%20The%20Animation%2003.mkv", subtitulo: "https://Dimagemfondo.github.io/visor-imgen/src/Hachishaku Hachiwa Keraku Meguri Igyou Kaikitan The Animation 02.es.ass" }
+        ]
+    }
+];
 
