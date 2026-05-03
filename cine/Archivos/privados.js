86f8a3c1776cc0922d6ec78f96eeaf99daf927be
Todesvip <maxterx40@gmail.com>
2026-05-03 15:54:50 -0500

    Update privados.js

--- cine/Archivos/privados.js
+++ cine/Archivos/privados.js
@@ -63,7 +63,6 @@ const privadosData = [
         titulo_jp: "家庭教師X催眠2 The Animation",
         poster: "https://noads-hue.github.io/noads/img/Kateikyoushi/poster.webp",
         poster1: "https://noads-hue.github.io/noads/img/Kateikyoushi/poster1.webp",
-        banner: "",
         logo: "https://noads-hue.github.io/noads/img/Kateikyoushi/logo.webp",
         rating: "18+",
         generos: ["Cosplay", "Juguetes"],
@@ -72,8 +71,8 @@ const privadosData = [
         calidad: "HD",
         sinopsis: "Basado en el juego erótico de POISON. ¡Las tutoras del personaje principal, Masato, son \"las hermanas sexualmente viciosas\" que conocía desde la infancia! Haruna Shinjo, la famosa hermana mayor, combina inteligencia, un par de cejas finas y el peor tipo de personalidad. Serina Shinjo, una estudiante universitaria con una personalidad y un cuerpo egoístas, está llena de lagunas. Ambos lo maltratan constantemente y ya está harto de su vida. Entonces, un día, Masato conoce a Sakiko Ichinose, la dueña del club secreto \"Gentildonna\", y le entrega un dispositivo de hipnosis. Usando el dispositivo, se le dice que puede manipular la mente y el cuerpo de cualquier persona a voluntad.",
         episodios: [
-            { num: 1, nombre: "Episodio 1", duracion: "24m", thumb: "", sprite: "", spriteImg: "", link: "https://ia600107.us.archive.org/28/items/jdiwued384d/Kateikyoushi X Saimin The Animation - 1.mp4", subtitulo: "https://noads-hue.github.io/noads/img/Kateikyoushi/Kateikyoushi x Saimin The Animation - 1.es.ass" },
-            { num: 2, nombre: "Episodio 2", duracion: "24m", thumb: "", sprite: "", spriteImg: "", link: "https://ia600107.us.archive.org/28/items/jdiwued384d/Kateikyoushi X Saimin The Animation - 2.mp4", subtitulo: "https://noads-hue.github.io/noads/img/Kateikyoushi/Kateikyoushi x Saimin The Animation - 2.es.ass" }
+            { num: 1, nombre: "Episodio 1", duracion: "24m", thumb: "https://noads-hue.github.io/noads/img/Kateikyoushi/01.jpg", sprite: "https://noads-hue.github.io/noads/img/Kateikyoushi/sprite1.vtt", spriteImg: "https://noads-hue.github.io/noads/img/Kateikyoushi/sprite1.jpg", link: "https://ia600107.us.archive.org/28/items/jdiwued384d/Kateikyoushi X Saimin The Animation - 1.mp4", subtitulo: "https://noads-hue.github.io/noads/img/Kateikyoushi/Kateikyoushi x Saimin The Animation - 1.es.ass" },
+            { num: 2, nombre: "Episodio 2", duracion: "24m", thumb: "https://noads-hue.github.io/noads/img/Kateikyoushi/02.jpg", sprite: "https://noads-hue.github.io/noads/img/Kateikyoushi/sprite2.vtt", spriteImg: "https://noads-hue.github.io/noads/img/Kateikyoushi/sprite2.jpg", link: "https://ia600107.us.archive.org/28/items/jdiwued384d/Kateikyoushi X Saimin The Animation - 2.mp4", subtitulo: "https://noads-hue.github.io/noads/img/Kateikyoushi/Kateikyoushi x Saimin The Animation - 2.es.ass" }
         ]
     },
     {
