c81c6a27b7b0d74134c1efb20d04779eb12cdfe7
Todesvip <maxterx40@gmail.com>
2026-05-10 00:57:24 -0500

    Remove multiple title entries from privados.js
    Removed multiple entries for various titles including '3Ping Lovers Ippu Nisai no Sekai e Youkoso The Animation', 'Cherry & Gals↑↑', and 'Dropout'.

--- cine/Archivos/privados.js
+++ cine/Archivos/privados.js
@@ -261,56 +261,6 @@ const privadosData = [
         ]
     },
     {
-        id: "3Ping",
-        titulo: "3Ping Lovers Ippu Nisai no Sekai e Youkoso The Animation",
-        titulo_jp: "3PingLovers!☆一夫二妻の世界へようこそ♪ THE ANIMATION",
-        poster: "https://dimagemfondo.github.io/visor-imgen/img/01/folder.jpg",
-        banner: "",
-        rating: "18+",
-        generos: ["Fantasía", "Harem"],
-        temporada: "Temporada 1",
-        ano: "2015",
-        calidad: "HD",
-        sinopsis: "Basado en el juego erótico de Hadashi Shoujo. Un Otaku atrapado en el mundo del juego RPG Eroge, cuando fue atacado por un monstruo, de repente fue salvado por la heroína que tenía. A partir de ese momento se lanzan a la aventura y disfrute del sexo harén.",
-        episodios: [
-            { num: 1, nombre: "Episodio 1", duracion: "24m", thumb: "", sprite: "https://Dimagemfondo.github.io/visor-imgen/sprite/03/sprite1.vtt", spriteImg: "https://Dimagemfondo.github.io/visor-imgen/sprite/03/sprite1.jpg", link: "https://ia801707.us.archive.org/34/items/sukebe-elf-tanbouki-1/3Ping%20Lovers%20Ippu%20Nisai%20no%20Sekai%20e%20Youkoso%20The%20Animation%20-%201.mkv", subtitulo: "https://Dimagemfondo.github.io/visor-imgen/src/3Ping Lovers Ippu Nisai no Sekai e Youkoso The Animation - 1.es.ass" }
-        ]
-    },
-    {
-        id: "Cherry",
-        titulo: "Cherry & Gals↑↑",
-        titulo_jp: "Cherry＆GAL’s↑↑",
-        poster: "https://veohentai.com/wp-content/uploads/2022/08/cherry-gal-s-2-x1.jpg",
-        banner: "",
-        rating: "18+",
-        generos: ["Incesto", "Colegialas"],
-        temporada: "Temporada 1",
-        ano: "2018",
-        calidad: "HD",
-        sinopsis: "Ruruna no se siente segura de su asociación Gyaru (término Gyaru utilizado para las jóvenes a la moda). Debido a que Ruruna nunca había tenido relaciones sexuales, no pudo participar charlando y contando historias con los amigos de Gyaru que siempre están hablando de problemas sexuales. Finalmente, Ria le ofrece a Ruruna tener sexo con el hermano menor de Ria, para que Ruruna pueda tener experiencia sobre sexo y pueda llegar a contar historias con otros amigos sobre sexo.",
-        episodios: [
-            { num: 1, nombre: "Episodio 1", duracion: "24m", thumb: "", sprite: "https://Dimagemfondo.github.io/visor-imgen/sprite/13/sprite1.vtt", spriteImg: "https://Dimagemfondo.github.io/visor-imgen/sprite/13/sprite1.jpg", link: "https://ia601707.us.archive.org/34/items/sukebe-elf-tanbouki-1/Cherry%20%26%20Gals%E2%86%91%E2%86%91%201.mkv", subtitulo: "https://Dimagemfondo.github.io/visor-imgen/src/Cherry & Gals↑↑ 1.es.ass" },
-            { num: 2, nombre: "Episodio 2", duracion: "24m", thumb: "", sprite: "https://Dimagemfondo.github.io/visor-imgen/sprite/13/sprite2.vtt", spriteImg: "https://Dimagemfondo.github.io/visor-imgen/sprite/13/sprite2.jpg", link: "https://ia601707.us.archive.org/34/items/sukebe-elf-tanbouki-1/Cherry%20%26%20Gals%E2%86%91%E2%86%91%202.mkv", subtitulo: "https://Dimagemfondo.github.io/visor-imgen/src/Cherry & Gals↑↑ 2.es.ass" }
-        ]
-    },
-    {
-        id: "Dropout",
-        titulo: "Dropout",
-        titulo_jp: "ドロップアウト",
-        poster: "https://noads-hue.github.io/noads/img/Dropout/poster.webp",
-        banner: "",
-        rating: "18+",
-        generos: ["Esclavas", "Colegialas", "Violación"],
-        temporada: "Temporada 1",
-        ano: "2016",
-        calidad: "HD",
-        sinopsis: "Después de reprobar sus exámenes de ingreso, la hermosa y talentosa estudiante de último año cae a un nuevo nivel y se convierte en una total esclava sexual.",
-        episodios: [
-            { num: 1, nombre: "Episodio 1", duracion: "24m", thumb: "https://noads-hue.github.io/noads/img/Dropout/01.jpg", sprite: "https://Dimagemfondo.github.io/visor-imgen/sprite/12/sprite1.vtt", spriteImg: "https://Dimagemfondo.github.io/visor-imgen/sprite/12/sprite1.jpg", link: "https://ia600107.us.archive.org/28/items/jdiwued384d/Dropout%201.mp4", subtitulo: "https://noads-hue.github.io/noads/img/Dropout/Dropout 1.es.ass" },
-            { num: 2, nombre: "Episodio 2", duracion: "24m", thumb: "https://noads-hue.github.io/noads/img/Dropout/02.jpg", sprite: "https://Dimagemfondo.github.io/visor-imgen/sprite/12/sprite2.vtt", spriteImg: "https://Dimagemfondo.github.io/visor-imgen/sprite/12/sprite2.jpg", link: "https://ia600107.us.archive.org/28/items/jdiwued384d/Dropout%202.mp4", subtitulo: "https://noads-hue.github.io/noads/img/Dropout/Dropout 2.es.ass" }
-        ]
-    },
-    {
         id: "Hachishaku",
         titulo: "Hachishaku Hachiwa Keraku Meguri: Igyou Kaikitan The Animation",
         titulo_jp: "",
